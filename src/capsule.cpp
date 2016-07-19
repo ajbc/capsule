d962dd296a8d1f2d2bf7a9ca6f624d9e7a36e23b
ajbc <ajbchaney@gmail.com>
2016-07-19 13:42:52 -0400

    Revert "updated SVI so the first pass initialized the latent variables (instead of the priors); this may not be effective"
    This reverts commit 3d09a56a1f90cbb95fac6450c86c7b7f1c14bb53.

--- src/capsule.cpp
+++ src/capsule.cpp
@@ -710,13 +710,11 @@ void Capsule::update_shape(int doc, int term, int count) {
 
 void Capsule::update_phi(int entity) {
     if (settings->svi) {
-        if (iter_count_entity[entity] > 1) {
-            double rho = pow(iter_count_entity[entity] + settings->delay,
-                -1 * settings->forget);
-            a_phi.col(entity) = (1 - rho) * a_phi_old.col(entity) + rho * a_phi.col(entity);
-            b_phi.col(entity) = (1 - rho) * b_phi_old.col(entity) + rho * b_phi.col(entity);
-        }
+        double rho = pow(iter_count_entity[entity] + settings->delay,
+            -1 * settings->forget);
+        a_phi.col(entity) = (1 - rho) * a_phi_old.col(entity) + rho * a_phi.col(entity);
         a_phi_old.col(entity) = a_phi.col(entity);
+        b_phi.col(entity) = (1 - rho) * b_phi_old.col(entity) + rho * b_phi.col(entity);
         b_phi_old.col(entity) = b_phi.col(entity);
     }
 
@@ -728,13 +726,11 @@ void Capsule::update_phi(int entity) {
 
 void Capsule::update_psi(int date) {
     if (settings->svi) {
-        if (iter_count_date[date] > 1) {
-            double rho = pow(iter_count_date[date] + settings->delay,
-                -1 * settings->forget);
-            a_psi(date) = (1 - rho) * a_psi_old(date) + rho * a_psi(date);
-            b_psi(date) = (1 - rho) * b_psi_old(date) + rho * b_psi(date);
-        }
+        double rho = pow(iter_count_date[date] + settings->delay,
+            -1 * settings->forget);
+        a_psi(date) = (1 - rho) * a_psi_old(date) + rho * a_psi(date);
         a_psi_old(date) = a_psi(date);
+        b_psi(date) = (1 - rho) * b_psi_old(date) + rho * b_psi(date);
         b_psi_old(date) = b_psi(date);
     }
 
@@ -744,13 +740,11 @@ void Capsule::update_psi(int date) {
 
 void Capsule::update_xi(int entity) {
     if (settings->svi) {
-        if (iter_count_entity[entity] > 1) {
-            double rho = pow(iter_count_entity[entity] + settings->delay,
-                -1 * settings->forget);
-            a_xi(entity) = (1 - rho) * a_xi_old(entity) + rho * a_xi(entity);
-            b_xi(entity) = (1 - rho) * b_xi_old(entity) + rho * b_xi(entity);
-        }
+        double rho = pow(iter_count_entity[entity] + settings->delay,
+            -1 * settings->forget);
+        a_xi(entity) = (1 - rho) * a_xi_old(entity) + rho * a_xi(entity);
         a_xi_old(entity) = a_xi(entity);
+        b_xi(entity) = (1 - rho) * b_xi_old(entity) + rho * b_xi(entity);
         b_xi_old(entity) = b_xi(entity);
     }
 
@@ -782,11 +776,9 @@ void Capsule::update_epsilon(int doc, int date) {
 
 void Capsule::update_beta(int iteration) {
     if (settings->svi) {
-        if (iteration > 1) {
-            double rho = pow(iteration + settings->delay,
-                -1 * settings->forget);
-            a_beta = (1 - rho) * a_beta_old + rho * a_beta;
-        }
+        double rho = pow(iteration + settings->delay,
+            -1 * settings->forget);
+        a_beta = (1 - rho) * a_beta_old + rho * a_beta;
         a_beta_old = a_beta * 1.0;
     }
 
@@ -803,11 +795,9 @@ void Capsule::update_beta(int iteration) {
 
 void Capsule::update_eta(int iteration) {
     if (settings->svi) {
-        if (iteration > 1) {
-            double rho = pow(iteration + settings->delay,
-                -1 * settings->forget);
-            a_eta = (1 - rho) * a_eta_old + rho * a_eta;
-        }
+        double rho = pow(iteration + settings->delay,
+            -1 * settings->forget);
+        a_eta = (1 - rho) * a_eta_old + rho * a_eta;
         a_eta_old = a_eta * 1.0;
     }
 
@@ -824,11 +814,9 @@ void Capsule::update_eta(int iteration) {
 
 void Capsule::update_pi(int date) {
     if (settings->svi) {
-        if (iter_count_date[date] > 1) {
-            double rho = pow(iter_count_date[date] + settings->delay,
-                -1 * settings->forget);
-            a_pi.row(date) = (1 - rho) * a_pi_old.row(date) + rho * a_pi.row(date);
-        }
+        double rho = pow(iter_count_date[date] + settings->delay,
+            -1 * settings->forget);
+        a_pi.row(date) = (1 - rho) * a_pi_old.row(date) + rho * a_pi.row(date);
         a_pi_old.row(date) = a_pi.row(date);
     }
 
