e32bbb100c2ba7538b94bf110c225540635c5b85
ajbc <ajbchaney@gmail.com>
2016-08-06 13:09:50 -0400

    added events data from historians

--- src/explore/eventness.py
+++ src/explore/eventness.py
@@ -1,35 +1,41 @@
-import numpy as np
-from scipy.special import digamma
-
-'''a = np.random.gamma(1.0,5.0, 10)
-print "raw"
-print a
-
-print"\nnormalized"
-na = a / sum(a)
-print na
-
-print "\n******\n\nlog raw"
-print digamma(a) - digamma(sum(a))
-
-print "\nlog normalized"
-print digamma(na) - digamma(sum(na))
-
-print digamma(na*sum(a)) - digamma(sum(na)*sum(a))'''
-
-import sys, sqlite3
+import sys, os
 import numpy as np
 from collections import defaultdict
 
-stem = "may23/weekly_bodyandsubj1976/capsule_dur0/"
-stem = "may23/weekly_bodyandsubj1976/exp3/"
-label = 'final'
+fit_dir = sys.argv[1]
 
-pi = np.loadtxt(stem + 'pi-' + label + '.dat')[:,1:]
-psi = np.loadtxt(stem + 'psi-' + label + '.dat')
-epsilon = np.loadtxt(stem + 'epsilon-' + label + '.dat')
-theta = np.loadtxt(stem + 'theta-' + label + '.dat')[:,1:]
-beta = np.loadtxt(stem + 'beta-' + label + '.dat')[:,1:]
+if sys.argv[2] == 'final':
+    iter_id = 'final'
+else:
+    iter_id = '%04d' % int(sys.argv[2])
+
+epsilon = np.loadtxt(os.path.join(fit_dir, 'epsilon-%s.dat' % iter_id))
+theta = np.loadtxt(os.path.join(fit_dir, 'theta-%s.dat' % iter_id))[:,1:]
+zeta = np.loadtxt(os.path.join(fit_dir, 'zeta-%s.dat' % iter_id))[:,1:]
+
+cumulative = np.sum(theta,1) + np.sum(zeta,1)
+for doc, time, eps, feps in epsilon:
+    cumulative[int(doc)] += feps
+
+
+eventness = defaultdict(float)
+dc = defaultdict(float)
+for doc, time, eps, feps in epsilon:
+    eventness[int(time)] += feps / cumulative[int(doc)]
+    dc[int(time)] += feps / eps
+
+for time in eventness:
+    print time, eventness[time], dc[time], (eventness[time]/dc[time])
+
+'''
+i0  284 5.076742e-01    4.167243e-02
+0   285 3.780185e-01    1.083041e-01
+0   286 1.998536e-01    1.998536e-01
+1   23  8.387790e-01    1.972619e-02
+1   24  7.229748e-01    5.934538e-02
+1   25  4.927697e-01    1.411809e-0
+
+for line in open(os.path.join(fit_dir, "eta-%s.dat" % iter_id)):
 docmap = {}
 for line in open("/home/statler/achaney/dat/DE/cable_data/pro/may23/weekly_bodyandsubj1976/doc_ids.tsv"):
     doc, idx = line.strip().split('\t')
@@ -58,4 +64,4 @@ for week in range(53):
         sc += scores[week][s] / counts[week][s]
         c +=1
     print week, sc, c, (sc/c)
-    #scores[week], counts[week], (scores[week]/counts[week])
+    #scores[week], counts[week], (scores[week]/counts[week])'''
